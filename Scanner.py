bf40edd12506530397918cce999096e3ee7d4d1b
Dvir Ben Asuli <dvir971@gmail.com>
2021-10-29 02:46:40 +0300

    COMPLETE and updated

--- Scanner.py
+++ Scanner.py
@@ -9,23 +9,23 @@ class Response(Enum):
     NOT_FOUND = 0
     FOUND = 1
 
+# BFS scan to check if a path exists between the source and destination nodes
 def scan_cube(graph, source, destination, xy, yz, zx):
-    sx = source[0]
-    sy = source[1]
-    sz = source[2]
     dx = destination[0]
     dy = destination[1]
     dz = destination[2]
 
     while not graph.queue.empty():
+        # BFS Scan- going over every possible direction from current placement and if
+        #           it's a valid position adding it to the queue for further checking
         curr_option = graph.queue.get()
 
         x = curr_option.x
         y = curr_option.y
         z = curr_option.z
         if x == dx and y == dy and z == dz:
+            # We reached the destination, scanning can stop
             return Response.FOUND, curr_option
-
 
         try_direction(graph, curr_option, x+1, y, z, xy, yz, zx)
         try_direction(graph, curr_option, x, y+1, z, xy, yz, zx)
@@ -34,9 +34,10 @@ def scan_cube(graph, source, destination, xy, yz, zx):
         try_direction(graph, curr_option, x, y-1, z, xy, yz, zx)
         try_direction(graph, curr_option, x, y, z-1, xy, yz, zx)
 
+    # If the queue is empty and we never reached destination- no path exists
     return Response.NOT_FOUND
 
-
+# Checking if we can move to a certain location
 def vacant_spot(x, y, z, xy, yz, zx):
     cond1 = xy[y][x] == 0
     cond2 = yz[z][y] == 0
@@ -44,6 +45,8 @@ def vacant_spot(x, y, z, xy, yz, zx):
 
     return cond1 and cond2 and cond3
 
+# Receives a possible step and in case its valid adds it to the graph
+# and to the queue for future BFS scan
 def try_direction(graph, curr_node, x, y, z, xy, yz, zx):
     if not vacant_spot(x, y, z, xy, yz, zx):
         return 0
@@ -52,6 +55,8 @@ def try_direction(graph, curr_node, x, y, z, xy, yz, zx):
     return 1
 
 
+# After we found a path, track the steps from destination to source
+# in reverse order (hence the LIFO)
 def track_path(grpah, destNode, sourceNode):
     path = LifoQueue(maxsize = grpah.xy_size*grpah.yz_size*grpah.zx_size)
 
@@ -64,6 +69,8 @@ def track_path(grpah, destNode, sourceNode):
 
     return path
 
+# Mapping every transitiion from one placement to the other to the
+# defined output format of the assignement
 def get_step(node):
     prev_node = node.get_parent()
 
--- oskar.py
+++ oskar.py
@@ -2,7 +2,6 @@
 # Dvir Ben Asuli        318208816
 # Tel-Aviv University   October 2021
 
-from queue import LifoQueue
 from Graph import Graph, Node
 from InstructionParser import instruction_parser
 from Scanner import Response, scan_cube, vacant_spot, track_path
@@ -26,19 +25,22 @@ class Main():
         root = Node(sx, sy, sz)
         graph = Graph(root, xy_mat, yz_mat, zx_mat)
 
+        # Checking destination point is valid
         if not vacant_spot(dx, dy, dz, xy_mat, yz_mat, zx_mat):
             print("No path to destination was found")
             return
 
+        # Scanning the cube as a graph
         response, dest_node = scan_cube(graph, source, destination, xy_mat, yz_mat, zx_mat)
+
         if response == Response.FOUND:
+            # If a path was found from source to destination, we need to track the steps
             steps = track_path(graph, dest_node, root)
 
             result_path = ""
             while not steps.empty():
                 result_path = result_path + str(steps.get()) + " "
 
-            print("Path to destination found:")
             print(f'{result_path}')
         else:
             print("No path to destination was found")
